2ce19c340e88ab1637c793dfbf9c4659f29ddbe0
Oskar Berntson <oskar.berntson@gmail.com>
2026-08-08 15:25:43 +0200

    About me page

--- js/about/about.js
+++ js/about/about.js
@@ -1,5 +1,7 @@
 import { header } from "../components/nav.js";
+import { footer } from "../components/footer.js";
 
 const headerContainer = document.getElementById("about-page-bg");
 
 header(headerContainer);
+footer();
